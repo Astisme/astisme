9dcacee7f18e6277dd661296cb703730a13597c6
Sir. Automation <auto@mate.com>
2024-02-11 19:12:24 +0100

    Reporting for duty

--- src/get_commits.mjs
+++ src/get_commits.mjs
@@ -1,21 +1,34 @@
-const myRepos = await fetch("https://api.github.com/users/Astisme/repos")
-console.log(myRepos);
-const latestCommits = [];
+import { Octokit } from "octokit"; 
+const octokit = new Octokit({ auth: process.env.TOKEN });
 
-for(const repo in myRepos){
-  const commits = await fetch(`https://api.github.com/repos/${repo.full_name}/commits`);
-  let i = 0;
-  while(i < commits.length() && i < 10){
-    const com = commits[i];
-    latestCommits.push(com);
-    i++;
-  }
+async function getRepos() {
+  //return await fetch("https://api.github.com/users/Astisme/repos");
+  return await octokit.request("GET https://api.github.com/users/Astisme/repos");
 }
 
-console.log({latestCommits});
-const sortedCommits = latestCommits.sort((a,b) => a.commit.author.date - b.commit.author.date).slice(0, 10);
-console.log({sortedCommits})
+async function getCommits(full_name) {
+  //return await fetch(`https://api.github.com/repos/${full_name}/commits`);
+  return await octokit.request(`GET https://api.github.com/repos/${full_name}/commits`);
+}
 
-/*for(const commit : sortedCommits){
-  
-}*/
+//async function run() {
+  const myRepos = await getRepos();
+  const latestCommits = [];
+
+  for(const repo in myRepos){
+    const commits = await getCommits(repo.full_name);
+    console.log({commits});
+    let i = 0;
+    for(const com in commits){
+      if(i >= 10) break;
+      latestCommits.push(com);
+      i++;
+    }
+  }
+
+  console.log({latestCommits,zero:latestCommits[0]});
+  const sortedCommits = latestCommits.sort((a,b) => a.commit.author.date - b.commit.author.date).slice(0, 10);
+  console.log({sortedCommits})
+//}
+
+//run();
